91a3c22eec4f3b7793df2549c506585d44b147c7
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-10 13:18:14 +0600

    Add C++ program to read and display input data

---
+++ Chapter-01/Example /1.3/4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+ using namespace std;
+ int main()
+ {
+
+    int i;
+    float f;
+    char s[80];
+
+
+    cout << "Enter a integer, float and string: ";
+    cin >> i >> f >> s;
+    cout << "your data is: ";
+
+    cout << i << " " << f <<" " << s;
+
+
+    return 0;
+ }
