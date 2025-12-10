708f8436da5660aeedd3f98f73d3a407a4a45059
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-10 13:22:59 +0600

    Add C++ program to sum two numbers

---
+++ Chapter-01/Example /1.6/2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b,s;
+
+    cout<<"Enter two numbers:"<<endl;
+    cin>>a>>b;
+
+    s=a+b;
+
+    cout<<"The sum is:"<<s;
+
+    return 0;
+
+}
