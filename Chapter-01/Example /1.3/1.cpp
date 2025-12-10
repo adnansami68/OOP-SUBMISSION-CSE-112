9ae3e7204a05145f9f25181432b0e42bc645f5e3
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-10 13:16:33 +0600

    Add initial C++ program to display variable values

---
+++ Chapter-01/Example /1.3/1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1;
+    int num2;
+    double num3;
+
+    num1=10;
+    num2=20;
+    num3=99.101;
+
+
+    cout<<"Here some values;"<<endl;
+    cout<<"num1 = "<<num1<<endl;
+    cout<<"num2 = "<<num2<<endl;
+    cout<<"num3 = "<<num3<<endl;
+
+
+    return 0;
+
+}
