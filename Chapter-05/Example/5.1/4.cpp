7b92009cafcacbb6e929b0d58e16623c3304edf7
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-11 21:04:23 +0600

    Create 4.cpp

---
+++ Chapter-05/Example/5.1/4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class myclass {
+    int x;
+
+public:
+
+    myclass() {
+        x = 0;
+    }
+
+    myclass(int n) {
+        x = n;
+    }
+
+    int getx() {
+        return x;
+    }
+};
+
+int main() {
+    myclass *p;
+    myclass ob(10);
+
+
+    p = new myclass[10];
+
+    if (!p) {
+        cout << "Allocation error";
+        return 1;
+    }
+
+
+    for (int i = 0; i < 10; i++)
+        p[i] = ob;
+
+
+    for (int i = 0; i < 10; i++) {
+        cout << "p[" << i << "]: " << p[i].getx() << '\n';
+    }
+
+
+    delete[] p;
+
+    return 0;
+}
