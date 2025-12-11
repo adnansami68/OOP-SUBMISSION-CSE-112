f0a37c5f8920a2406325652b07ef2b6e0b1a0086
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-11 21:05:26 +0600

    Create 2.cpp

---
+++ Chapter-05/Example/5.2/2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+class strtype {
+    char *p;
+
+public:
+
+    strtype(char *s);
+
+
+    ~strtype() {
+        delete[] p;
+    }
+
+    char *get() {
+        return p;
+    }
+};
+
+
+strtype::strtype(char *s) {
+    int l = strlen(s) + 1;
+    p = new char[l];
+    if (!p) {
+        cout << "Allocation error";
+        exit(1);
+    }
+    strcpy(p, s);
+}
+
+
+void show(strtype x) {
+    char *s = x.get();
+    cout << s << "\n";
+}
+
+int main() {
+    strtype a("Hello"), b("There");
+
+    show(a);
+    show(b);
+
+    return 0;
+}
