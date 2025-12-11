e2a3ad2dc6cb67b4f9649d56ab3037efdabfb3d2
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-11 20:54:10 +0600

    Create 1.cpp

---
+++ Chapter-04/Example/4.3/1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class inventory
+{
+    char item[20];
+    double cost;
+    int on_hand;
+
+public:
+
+    inventory(char *i, double c, int o)
+{
+    strcpy(item, i);
+    cost = c;
+    on_hand = o;
+}
+    void show();
+};
+    void inventory::show()
+{
+    cout << item;
+    cout << ": $" << cost;
+    cout << " On hand: " << on_hand << "\n";
+}
+ int main()
+{
+    inventory ob("wrench", 5.95, 4);
+    ob.show();
+
+    return 0;
+ }
