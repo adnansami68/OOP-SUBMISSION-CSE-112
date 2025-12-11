127888732783360c1f5d63a99828060595b6d877
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-11 20:50:02 +0600

    Create 1.cpp

---
+++ Chapter-04/Example/4.1/1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+class Norml
+ {
+    int a;
+
+public:
+
+    void set_a(int n) { a = n; }
+
+int get_a() { return a; }
+ };
+
+
+    int main()
+ {
+
+    Norml ob[4];
+    int i;
+
+for(i=0; i<4; i++)
+
+    ob[i].set_a(i);
+
+for(i=0; i<4; i++)
+
+    cout << ob[i].get_a();
+    cout << "\n";
+
+    return 0;
+
+ }
