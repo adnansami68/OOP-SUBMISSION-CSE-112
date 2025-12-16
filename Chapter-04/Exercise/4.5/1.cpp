ad436f0914e6e965431122f79ce497768412d898
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2025-12-16 12:52:32 +0600

    Create 1.cpp

---
+++ Chapter-04/Exercise/4.5/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+
+    char *p = new char[100];
+
+
+    strcpy(p, "This is a test");
+
+
+    cout << p << endl;
+
+    delete[] p;
+
+    return 0;
+}
